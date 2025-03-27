c4870cc4e1bc60a9b425abbbfb948656d9f7720e
inI7ain <inI7ain@pm.me>
2025-03-27 18:16:59 +0100

    https magic

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 import { Application } from 'https://deno.land/x/oak/mod.ts'
 import router from "./router.ts";
 
+const cert = await Deno.readTextFile("./certificates/cert.pem");
+const key = await Deno.readTextFile("./certificates/key.pem");
+
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-await app.listen({ port: 8000 });
+await app.listen({ 
+  port: 8000,
+  secure: true,
+  cert,
+  key
+ });
--- router.ts
+++ router.ts
@@ -5,6 +5,10 @@ const router = new Router();
 router.get("/users/:id", async (context) => {
     const userId = context.params.id;
     context.response.body = { userId, message: "User data extracted successfully."} 
+});
+
+router.use("/favicon.ico", () => {
+    return new Response(null, { status: 204 });
 })
 
 export default router;
